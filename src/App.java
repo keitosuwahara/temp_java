52c0b721b8270875b4f54f5abb71a3780280e10e
keitosuwahara <210103gi@yse-c.net>
2023-01-08 10:38:59 +0900

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for(int i = 0; i<10; i++) {
+            System.out.println(i);
+            }
+        }
     }
-}
+    
+
