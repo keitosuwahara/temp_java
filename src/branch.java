36311d31ae33a4bad78d2e417b38501d40352366
keitosuwahara <210103gi@yse-c.net>
2023-01-12 07:00:34 +0900

    up

---
+++ src/branch.java
@@ -0,0 +1,11 @@
+
+
+public class branch {
+public static void main(String[] args) {
+
+System.out.println(Culc.tasu(4, 44));
+System.out.println(Culc.hiku(4, 2));
+
+}
+
+}
---
+++ src/culc.java
@@ -0,0 +1,10 @@
+
+public class Culc {
+public static int tasu(int x, int y) {
+    return x+y;
+
+    }
+public static int hiku(int x, int y) {
+    return x-y;
+}
+}
