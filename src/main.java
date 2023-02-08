96bcbd9e08964d5c398813a9516c0f712ca1ec3a
keitosuwahara <210103gi@yse-c.net>
2023-02-08 12:20:05 +0900

    u

--- src/main.java
+++ src/main.java
@@ -1,15 +1,44 @@
-import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class main {
 public static void main(String[] args) {
 
-int[] ar = {55,44,33,22,11};
-Arrays.sort(ar);
+    /* 
+    Date now = new Date();
+    Calendar c = Calendar.getInstance();
+    c.setTime(now);
+    int year = c.get(Calendar.YEAR);
+    int month = c.get(Calendar.MONTH);
+    int day = c.get(Calendar.DATE);
+    int hour = c.get(Calendar.HOUR);
+    int min = c.get(Calendar.MINUTE);
+    int sec =  c.get(Calendar.SECOND);
+    
+    month+=1;
 
-for(int s : ar) {
-    System.out.println(s);
+
+    //System.out.println("現在は"+year+"年"+month+"月"+day+"日"+hour+"時"+min+"分"+sec+"秒");
+
+SimpleDateFormat f = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒");
+String s = f.format(now);
+System.out.println(s);
+*/
+
+/*tryの後()で囲むとclose()を書かなくてもいい => ファイル処理やDB処理など*/
+try (FileWriter file = new FileWriter("../txt/test.txt");){ 
+    file.write("okokokok");
+
+}catch(IOException e) {
+    System.out.println("[INFO]  "+e.getMessage());
+
+}catch(NullPointerException e) {//fileがnullだった場合
+    System.out.println("[INFO]  "+e.getMessage());
+
+}finally {
+    System.out.println("finallyは何があろうと最後に必ず実行される");
+
+}
+}
 }
 
-}
-
-}
