e0aee6ffa715b7d89e7b83c5093e8e1c21c57fe5
keitosuwahara <210103gi@yse-c.net>
2023-02-10 11:25:13 +0900

    up

--- src/main.java
+++ src/main.java
@@ -1,44 +1,26 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class main {
 public static void main(String[] args) {
 
-    /* 
-    Date now = new Date();
-    Calendar c = Calendar.getInstance();
-    c.setTime(now);
-    int year = c.get(Calendar.YEAR);
-    int month = c.get(Calendar.MONTH);
-    int day = c.get(Calendar.DATE);
-    int hour = c.get(Calendar.HOUR);
-    int min = c.get(Calendar.MINUTE);
-    int sec =  c.get(Calendar.SECOND);
-    
-    month+=1;
+Map<String, String> map = new HashMap<>();
+map.put("国語", "30");
+map.put("社会", "70");
+map.put("英語", "56");
 
+for(Map.Entry<String, String> entry:map.entrySet()) {
+    System.out.println(entry.getKey()+":"+entry.getValue());
+    }
 
-    //System.out.println("現在は"+year+"年"+month+"月"+day+"日"+hour+"時"+min+"分"+sec+"秒");
+int[] ars = {11,22,33,44,55};
 
-SimpleDateFormat f = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒");
-String s = f.format(now);
-System.out.println(s);
-*/
-
-/*tryの後()で囲むとclose()を書かなくてもいい => ファイル処理やDB処理など*/
-try (FileWriter file = new FileWriter("../txt/test.txt");){ 
-    file.write("okokokok");
-
-}catch(IOException e) {
-    System.out.println("[INFO]  "+e.getMessage());
-
-}catch(NullPointerException e) {//fileがnullだった場合
-    System.out.println("[INFO]  "+e.getMessage());
-
-}finally {
-    System.out.println("finallyは何があろうと最後に必ず実行される");
+for (int i = 0 ; i < ars.length ; i++) {
+    System.out.println(ars[i]);
 
 }
 }
 }
+
+
 
